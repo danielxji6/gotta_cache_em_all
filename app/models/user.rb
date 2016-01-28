9e79b355f4a7d0395f9e8917d05e508f047ad220
jroers <joe.roers@gmail.com>
2016-01-28 15:15:42 -0800

    fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	def self.confirm(params)
-		@user = User.find_by({username: parmas[:username]})
+		@user = User.find_by({username: params[:username]})
 		@user.try(:authenticate, params[:password])
 	end
 
