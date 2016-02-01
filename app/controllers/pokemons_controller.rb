00a4e3866639faf659282affe44fffc46a639ead
jroers <joe.roers@gmail.com>
2016-02-01 10:39:07 -0800

    Changed up pokemons#create method to check that the user has not already collected an event

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -17,22 +17,39 @@ class PokemonsController < ApplicationController
 	end
 
 	def create
-		user_params = params.require(:user).permit(:username, :password)
+		
+		# Collects hash information to find the corresponding event
 		hash_info = params.require(:user).permit(:hash_data)
-		# puts hash_info.hash_data
 		@event = Event.find_by(hash_info)
-		@user = User.confirm(user_params)
-		puts "User:::" + @user.to_s + "         Event:::" + @event.to_s
+		
+		# Checks whether the user is already logged in or submitted the login form.
+		if session[:user_id] == nil
+			user_params = params.require(:user).permit(:username, :password)
+			@user = User.confirm(user_params)
+			puts "User:::" + @user.to_s + "         Event:::" + @event.to_s
+		else
+			@user = User.find_by_id(session[:user_id])
+		end
+
+		# Verifies that the user and the event are both valid
 		if @user && @event
 			login(@user)
-			poke = Pokemon.create(level: rand(@event.level_min..@event.level_max))
-			@user.pokemons << poke
-			@event.pokemons << poke
-			redirect_to @user
-	    	flash[:notice] = "Successfully caught pokemon!"
+			# Checks to see if the user has not already collected the event.
+			if Pokemon.where(user_id: @user.id, event_id: @event.id).length == 0
+				poke = Pokemon.create(level: rand(@event.level_min..@event.level_max))
+				@user.pokemons << poke
+				@event.pokemons << poke
+				redirect_to @user
+		    flash[:notice] = "Successfully caught pokemon!"
+	    # If already collected, redirect, do not catch, and flash a notice
+		  else
+		  	redirect_to @user
+		  	flash[:notice] = "You've already collected this event!"
+		  end
+	  # If either the user or event is invalid, redirect to the catch page to try again
 		else
-			redirect_to login_path
-			flash[:error] = "Invalid credentials. Please try again."
+			redirect_to catch_path(@event.hash_data)
+			flash[:error] = "Invalid user credentials. Please try again."
 		end
 	end
 
