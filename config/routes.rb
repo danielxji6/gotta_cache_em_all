32618db197a65fde756ffd205df4c5ed848114ee
tscurrie <tscurrie85@gmail.com>
2016-02-03 12:27:19 -0800

    made it so you cant go to login page if youre logged in

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
 
 
-  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#new', as: 'login'
   get '/logout', to: 'sessions#destroy'
   post '/sessions', to: 'sessions#create'
 
