680f42b6e3f9625253d1f8525557932af57aad47
jroers <joe.roers@gmail.com>
2016-02-03 14:44:49 -0800

    Added description to events#create, updated styling on index.html.erb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -88,7 +88,7 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:dex_number, :coords, :level_min, :level_max)
+    params.require(:event).permit(:dex_number, :coords, :level_min, :level_max, :description)
   end
 
   def set_hash_data
