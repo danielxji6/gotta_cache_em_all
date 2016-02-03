6797404c0741a2e8aed0cf6adae610100d44057f
jroers <joe.roers@gmail.com>
2016-02-03 15:40:57 -0800

    Added anti-seed information

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Pokemon.destroy_all
+User.destroy_all
+Event.destroy_all
+# Hope.destroy_all
 
 user1_data = {
 	full_name: "Test1",
