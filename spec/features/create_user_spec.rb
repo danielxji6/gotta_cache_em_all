b7d657b0471f30defe962ad5d9151567b3ec6574
Daniel Lwo <danielxji6@gmail.com>
2016-02-02 22:35:07 -0800

    add class to flash

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -20,21 +20,19 @@ describe "creating a new user" do
     click_button 'Sign up'
 
     # verify we're on a page like /user/:id
-    expect(page.current_path).to match(/products\/\d+?$/)
+    expect(page.current_path).to match(/events/)
 
-    expect(page).to have_content 'Test Tom'
-    expect(page).to have_content ''
-    expect(page).to have_content 'asdf1234'
-    expect(page).to have_content '2.40'
-    expect(page).to have_content '27.00'
   end
 
   context 'when the form is incomplete' do
     it 'displays an error message' do
-      click_button 'Save Product'
-      expect(page.find('.alert-danger')).to have_content "Name can't be blank, Description can't be blank,"
-      expect(page.find('.alert-danger')).to have_content "Category can't be blank, Sku can't be blank,"
-      expect(page.find('.alert-danger')).to have_content " Wholesale can't be blank, Retail can't be blank"
+      fill_in 'user_full_name', with: 'Test Tom'
+      fill_in 'user_username', with: 'testntest'
+      fill_in 'user_email', with: 'tt@tt.ga'
+      fill_in 'user_password', with: '123'
+      click_button 'Sign up'
+      expect(page.find('.alert-danger')).to have_content "Username has already been take"
+      expect(page.find('.alert-danger')).to have_content "Email has already been taken"
     end
   end
 end
