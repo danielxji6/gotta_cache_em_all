0652d4d7939de5734d115914a28447117b089266
Daniel Lwo <danielxji6@gmail.com>
2016-02-03 16:14:47 -0800

    fix testing error

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -16,7 +16,7 @@ describe 'Creating a new event' do
   end
 
   it 'saves a new event' do
-    page.find_by_id('event_dex_number').select(25)
+    page.find_by_id('event_dex_number').select('Pikachu')
     fill_in 'event_coords', with: '37.7577627,-122.4726194'
     fill_in 'event_level_min', with: 10
     fill_in 'event_level_max', with: 20
@@ -25,6 +25,7 @@ describe 'Creating a new event' do
 
     expect(page.current_path).to match(/events\/\d+?$/)
 
+    expect(page).to have_content('Pikachu')
     expect(page).to have_content('37.7577627,-122.4726194')
 
   end
