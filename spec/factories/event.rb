27de21cfd87503b3494a7312a49e66cb6cd41c52
danielxji6 <danielxji6@gmail.com>
2016-03-14 20:49:19 -0700

    add testing: editing event

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :event, class: Event do
   	name { "Pikachu" }
-  	coords { "123 }123" }
+  	coords { "123,123" }
   	category { "Electric" }
   	dex_number { 25 }
-  	description { "Pika } pika. Find it next to the cashier" }
+  	description { "Pika. pika. Find it next to the cashier" }
   	image { "https://s3-eu-west-1.amazonaws.com/calpaterson-pokemon/25.jpeg" }
   	level_min { 10 }
   	level_max { 20 }
--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-describe 'Creating a new event' do
+describe 'Creating and a new event' do
 
   let!(:admin) { FactoryGirl.create(:admin) }
+
   before do
     visit '/login'
     fill_in 'user_username', with: admin.username
@@ -27,6 +28,37 @@ describe 'Creating a new event' do
 
     expect(page).to have_content('Pikachu')
     expect(page).to have_content('37.7577627,-122.4726194')
+
+  end
+end
+
+describe 'Editing a event' do
+
+  let!(:event) { FactoryGirl.create(:event) }
+  let!(:admin) { FactoryGirl.create(:admin) }
+
+  before do
+    visit '/login'
+    fill_in 'user_username', with: admin.username
+    fill_in 'user_password', with: admin.password
+    click_button 'Login'
+    visit edit_event_path(event)
+  end
+
+  it 'shows a form' do
+    expect(page).to have_css('form')
+  end
+
+  it 'saves a event edit data' do
+    fill_in 'event_coords', with: '123,-111'
+    fill_in 'event_level_min', with: 10
+    fill_in 'event_level_max', with: 99
+    fill_in 'event_description', with: 'Pika Pika ka ka ka'
+    click_button 'Update Event'
+
+    expect(page.current_path).to match(/events\/\d+?$/)
+
+    expect(page).to have_content('123,-111')
 
   end
 
